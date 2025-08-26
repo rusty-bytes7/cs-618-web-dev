608efe52b1f4837db6a4fe0b0114f0d5ddee5456
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2025-08-25 20:48:05 -0400

    initial react project files

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import rusty from './assets/rusty.png'
+import rusty2 from '/Users/ericawolf/vscode/hello-codespaces/src/assets/IMG_2791.JPG'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={rusty2} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={rusty} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Rusty is so cute!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          You have pet Rusty {count} times!
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
