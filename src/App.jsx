5de4c19073a3502436e32c7c8c8d47134d0035ff
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2025-08-29 15:28:14 -0400

    chore: configure commitlint

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import rusty from './assets/rusty.png'
 import rusty2 from '/Users/ericawolf/vscode/hello-codespaces/src/assets/IMG_2791.JPG'
 import './App.css'
@@ -11,15 +9,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={rusty2} className="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+          <img src={rusty2} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={rusty} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank' rel='noreferrer'>
+          <img src={rusty} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Rusty is so cute!</h1>
-      <div className="card">
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           You have pet Rusty {count} times!
         </button>
@@ -27,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
