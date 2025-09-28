3fdfb6dfe134b05eedfd374532036c45d7e6ff5b
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2025-09-28 15:03:46 -0400

    build: added login features and updated testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import { userRoutes } from './routes/users.js'
 
 import { postsRoutes } from './routes/posts.js'
 
@@ -9,6 +10,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 postsRoutes(app)
+userRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express Live!')
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { PostList } from './components/PostList.jsx'
-import { CreatePost } from './components/CreatePost.jsx'
-import { PostFilter } from './components/PostFilter.jsx'
-import { PostSorting } from './components/PostSorting.jsx'
-import { getPosts } from './api/posts.js'
+import { PostList } from '../components/PostList.jsx'
+import { CreatePost } from '../components/CreatePost.jsx'
+import { PostFilter } from '../components/PostFilter.jsx'
+import { PostSorting } from '../components/PostSorting.jsx'
+import { Header } from '../components/Header.jsx'
+import { getPosts } from '../api/posts.js'
 
 export function Blog() {
   const [author, setAuthor] = useState('')
@@ -19,7 +20,10 @@ export function Blog() {
 
   return (
     <div style={{ padding: 8 }}>
-      <h1>Welcome to my Blog</h1>
+      <Header />
+      <br />
+      <hr />
+      <br />
       <CreatePost />
       <br />
       <hr />
