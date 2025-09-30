72ea3efc2e65a919755ad550dacd52037059023f
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2025-09-29 21:01:29 -0400

    chore: changing routes/users.js line 18

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -15,7 +15,7 @@ export function userRoutes(app) {
   app.post('/api/v1/user/login', async (req, res) => {
     try {
       const token = await loginUser(req.body)
-      return res.status(200).send({ token })
+      return res.status(200).json({ token })
     } catch (err) {
       return res.status(400).send({
         error: 'login failed, did you enter the correct username/password?',
