3ff62e040454c2a9f5c77a88e1e7e134a2ff4b56
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2025-10-13 14:18:28 -0400

    build: added events to front and backend to collect user engagement

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { userRoutes } from './routes/users.js'
+import { eventRoutes } from './routes/events.js'
 
 import { postsRoutes } from './routes/posts.js'
 
@@ -11,6 +12,7 @@ app.use(bodyParser.json())
 
 postsRoutes(app)
 userRoutes(app)
+eventRoutes(app)
 
 app.get('/', (req, res) => {
   res.send('Hello from Express Live!')
--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation } from '@tanstack/react-query'
 import { Header } from '../components/Header.jsx'
 import { Post } from '../components/Post.jsx'
 import { getPostById } from '../api/posts.js'
+import { useEffect, useState } from 'react'
+import { postTrackEvent } from '../api/events.js'
 import { getUserInfo } from '../api/users.js'
 import { Helmet } from 'react-helmet-async'
 
 export function ViewPost({ postId }) {
+  const [session, setSession] = useState()
+  const trackEventMutation = useMutation({
+    mutationFn: (action) => postTrackEvent({ postId, action, session }),
+    onSuccess: (data) => setSession(data?.session),
+  })
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      trackEventMutation.mutate('startView')
+      timeout = null
+    }, 1000)
+    return () => {
+      if (timeout) clearTimeout(timeout)
+      else trackEventMutation.mutate('endView')
+    }
+  }, [])
   const postQuery = useQuery({
     queryKey: ['post', postId],
     queryFn: () => getPostById(postId),
